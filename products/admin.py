691953095c4bec63fd9a5f27bd118999b4636136
MCamish29 <mcamish29@gmail.com>
2025-03-23 13:09:25 +0000

    add product models, admin and media

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import Category, Country, Tea, Equipment, Kit
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'category_id',
+        'category',
+        'created',
+        'updated',
+    )
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'country_id',
+        'country',
+        'created',
+        'updated',
+    )
+
+
+class TeaAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'internal_name',
+        'product_name',
+        'weight',
+        'category',
+        'price',
+        'created',
+        'updated',
+    )
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'internal_name',
+        'product_name',
+        'category',
+        'price',
+        'created',
+        'updated',
+    )
+
+
+class KitAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'internal_name',
+        'product_name',
+        'category',
+        'price',
+        'created',
+        'updated',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Tea, TeaAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(Kit, KitAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,85 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    category_id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=50)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.category 
+
+
+class Country(models.Model):
+    class Meta:
+        verbose_name_plural = 'Countries'
+    country_id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=75)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.country
+
+
+class Tea(models.Model):
+    class Meta:
+        verbose_name_plural = 'Tea'
+    
+    # Using UUID as a unique identifier across all models
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
+    internal_name = models.CharField(max_length=250)
+    product_name = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField()
+    blend = models.TextField()
+    weight = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    country_of_origin = models.ForeignKey(Country, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.internal_name
+
+
+class Equipment(models.Model):
+    class Meta:
+        verbose_name_plural = 'Equipment'
+    
+    # Using UUID as a unique identifier across all models
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
+    internal_name = models.CharField(max_length=250)
+    product_name = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    country_of_origin = models.ForeignKey(Country, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.internal_name
+
+
+class Kit(models.Model):
+    # Using UUID as a unique identifier across all models
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
+    internal_name = models.CharField(max_length=250)
+    product_name = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.internal_name
