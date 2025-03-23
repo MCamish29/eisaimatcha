32b065e6abacba21902461ad6cce8fdc9067796c
MCamish29 <mcamish29@gmail.com>
2025-03-23 13:29:34 +0000

    add product views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Tea, Equipment, Kit
 
-# Create your views here.
+def all_products(request):
+    """A view to return all products page"""
+    
+    # Fetch all products
+    teas = Tea.objects.all()
+    equipment = Equipment.objects.all()
+    kits = Kit.objects.all()
+    
+    # Combine all products into one list (optional, if you want to display them all together)
+    all_products = list(teas) + list(equipment) + list(kits)
+    
+    context = {
+        'all_products': all_products,
+    }
+    
+    return render(request, 'products/products.html', context)
