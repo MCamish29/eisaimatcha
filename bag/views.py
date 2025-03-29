db740f7c68692893f15bf6ab003efe03e7b6d1e8
MCamish29 <mcamish29@gmail.com>
2025-03-29 15:59:46 +0000

    add toast views and templates

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Tea, Equipment, Kit
 
 # Create your views here.
 def view_bag(request):
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-
+  
+    product = None
+    
+    try:
+        product = Tea.objects.get(pk=item_id)
+    except Tea.DoesNotExist:
+        try:
+            product = Equipment.objects.get(pk=item_id)
+        except Equipment.DoesNotExist:
+            try:
+                product = Kit.objects.get(pk=item_id)
+            except Kit.DoesNotExist:
+                messages.error(request, "Product not found!")
+                return redirect('view_bag')
+            
+            
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag  = request.session.get('bag', {})
@@ -14,6 +32,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.product_name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
