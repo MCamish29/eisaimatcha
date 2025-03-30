e22b3bfae4b9efe367b0be2da15e0779f320d3e3
MCamish29 <mcamish29@gmail.com>
2025-03-30 10:48:07 +0100

    add toast messages to product views

--- bag/views.py
+++ bag/views.py
@@ -30,6 +30,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.product_name} to your bag')
@@ -39,26 +40,58 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
 
+    product = None
+    
+    try:
+        product = Tea.objects.get(pk=item_id)
+    except Tea.DoesNotExist:
+        try:
+            product = Equipment.objects.get(pk=item_id)
+        except Equipment.DoesNotExist:
+            try:
+                product = Kit.objects.get(pk=item_id)
+            except Kit.DoesNotExist:
+                messages.error(request, "Product not found!")
+                return redirect('view_bag')
+
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.product_name} from bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 def remove_from_bag(request, item_id):
 
+    product = None
+    
+    try:
+        product = Tea.objects.get(pk=item_id)
+    except Tea.DoesNotExist:
+        try:
+            product = Equipment.objects.get(pk=item_id)
+        except Equipment.DoesNotExist:
+            try:
+                product = Kit.objects.get(pk=item_id)
+            except Kit.DoesNotExist:
+                messages.error(request, "Product not found!")
+                return redirect('view_bag')
+
     try:
         bag = request.session.get('bag', {})
 
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.product_name} from bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
 
