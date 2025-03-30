3bc256c59294e838e291b0b29af6bf934bb79acd
MCamish29 <mcamish29@gmail.com>
2025-03-30 12:12:05 +0100

    create checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,81 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Tea, Equipment, Kit
 
 # Create your models here.
+
+class Order(models.Model):
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+
+        return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+
+    def save(self, *args, **kwargs):
+        
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    tea = models.ForeignKey(Tea, null=True, blank=True, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, null=True, blank=True, on_delete=models.CASCADE)
+    kit = models.ForeignKey(Kit, null=True, blank=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+    # Check which product is selected and calculate lineitem_total accordingly
+        if self.tea:
+            self.lineitem_total = self.tea.price * self.quantity
+        elif self.equipment:
+            self.lineitem_total = self.equipment.price * self.quantity
+        elif self.kit:
+            self.lineitem_total = self.kit.price * self.quantity
+        else:
+            self.lineitem_total = 0
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        
+        if self.tea:
+            return f'{self.tea.product_id} on order {self.order.order_number}'
+        elif self.equipment:
+            return f'{self.equipment.product_id} on order {self.order.order_number}'
+        elif self.kit:
+            return f'{self.kit.product_id} on order {self.order.order_number}'
+        else:
+            return f'Unknown product on order {self.order.order_number}'
