87d1989bb06ddb1a8d5513df08b9fe5756bc8013
MCamish29 <mcamish29@gmail.com>
2025-03-31 15:10:14 +0100

    create confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,12 +5,33 @@ import time
 from .models import Order, OrderLineItem
 from products.models import Tea, Equipment, Kit
 from profiles.models import UserProfile
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 class StripeWH_Handler:
     """Handle Stripe Webhooks"""
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
 
     def handle_event(self, event):
         """
@@ -98,6 +119,7 @@ class StripeWH_Handler:
                 time.sleep(1)
 
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200
@@ -148,9 +170,8 @@ class StripeWH_Handler:
                 order.delete()
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | ERROR: {e}',
-                status=500
-            )
-
+                status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Order created',
             status=200
