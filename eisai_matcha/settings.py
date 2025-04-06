b5da9df0b92368e994507968ddba5e3c93df0a55
MCamish29 <mcamish29@gmail.com>
2025-04-06 12:57:35 +0100

    update AWS settings

--- eisai_matcha/settings.py
+++ eisai_matcha/settings.py
@@ -180,8 +180,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'eisaibucket'
     AWS_S3_REGION_NAME = 'us-east-1'
-    AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
